6662f86765ab7a28b5332b36317ada3892a0f1b5
lhall383 <lucashall383@gmail.com>
2022-04-07 01:56:13 -0500

    Handle actions and upgrades.
    Sort possible actions by priority and execute highest priority action.
    Buy cheapest skill upgrade any time we can afford it.

--- src/daemons/bladeburner-daemon.ts
+++ src/daemons/bladeburner-daemon.ts
@@ -1,5 +1,12 @@
 import { NS } from "@ns";
 
+interface ActionOption {
+  type: string;
+  name: string;
+  priority: number;
+  duration: number;
+}
+
 export async function main(ns: NS): Promise<void> {
   const args = ns.flags([["loop", true]]);
 
@@ -14,13 +21,97 @@ export async function main(ns: NS): Promise<void> {
   }
 
   do {
+    // handle upgrades
+    const skillOptions = ns.bladeburner.getSkillNames().map((name) => {
+      return { name, cost: ns.bladeburner.getSkillUpgradeCost(name) };
+    });
+    skillOptions.sort((a, b) => a.cost - b.cost);
+    if (
+      skillOptions.length > 0 &&
+      ns.bladeburner.getSkillPoints() > skillOptions[0].cost
+    ) {
+      ns.bladeburner.upgradeSkill(skillOptions[0].name);
+      ns.print(
+        `Upgrading ${skillOptions[0].name} for ${skillOptions[0].cost} SP`
+      );
+    }
+
     const [currentStamina, maxStamina] = ns.bladeburner.getStamina();
     const lowStamina = maxStamina * 0.5;
-    const highStamina = maxStamina * 0.95;
 
-    ns.print(`Stamina: ${currentStamina}/${maxStamina}`);
-    ns.print(`Thresholds: [${lowStamina} - ${highStamina}]`);
+    ns.print(
+      `Stamina: ${ns.nFormat(
+        (currentStamina / maxStamina) * 100,
+        "0.0"
+      )}% ${ns.nFormat(currentStamina, "0.00")}/${ns.nFormat(
+        maxStamina,
+        "0.00"
+      )}`
+    );
 
-    await ns.sleep(1000);
+    // handle action
+    const actionOptions = [] as ActionOption[];
+    if (currentStamina < lowStamina) {
+      actionOptions.push({
+        type: "general",
+        name: "Field Analysis",
+        priority: 1.0,
+        duration: ns.bladeburner.getActionTime("general", "Field Analysis"),
+      });
+    } else {
+      actionOptions.push({
+        type: "contracts",
+        name: "Tracking",
+        priority:
+          ns.bladeburner.getActionCountRemaining("contracts", "Tracking") > 0
+            ? 1.0
+            : 0.0,
+        duration: ns.bladeburner.getActionTime("contracts", "Tracking"),
+      });
+      actionOptions.push({
+        type: "contracts",
+        name: "Retirement",
+        priority:
+          ns.bladeburner.getActionCountRemaining("contracts", "Retirement") > 0
+            ? 0.8
+            : 0.0,
+        duration: ns.bladeburner.getActionTime("contracts", "Retirement"),
+      });
+      actionOptions.push({
+        type: "contracts",
+        name: "Bounty Hunter",
+        priority:
+          ns.bladeburner.getActionCountRemaining("contracts", "Bounty Hunter") >
+          0
+            ? 0.6
+            : 0.0,
+        duration: ns.bladeburner.getActionTime("contracts", "Bounty Hunter"),
+      });
+    }
+
+    // sort action options, highest priority first
+    actionOptions.sort((a, b) => b.priority - a.priority);
+    if (actionOptions.length === 0) {
+      ns.print("No actions added to priorities");
+      await ns.sleep(100);
+      continue;
+    }
+
+    const bestAction = actionOptions[0];
+    const started = ns.bladeburner.startAction(
+      bestAction.type,
+      bestAction.name
+    );
+    if (started) {
+      ns.print(
+        `Action ${bestAction.type} ${bestAction.name} started for ${bestAction.duration}ms`
+      );
+      await ns.sleep(bestAction.duration);
+    } else {
+      ns.print(`Action ${bestAction.type} ${bestAction.name} failed to start`);
+      await ns.sleep(100);
+    }
+
+    await ns.sleep(1);
   } while (args["loop"]);
 }
